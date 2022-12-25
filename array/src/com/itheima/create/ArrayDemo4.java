c615f0e31c1e4e573c3eb71f2c30700a28abe8bd
cczhulala <113978469+cczhulala@users.noreply.github.com>
2022-12-25 16:36:21 +0800

    Update ArrayDemo4.java

--- array/src/com/itheima/create/ArrayDemo4.java
+++ array/src/com/itheima/create/ArrayDemo4.java
@@ -1,11 +1,5 @@
 package com.itheima.create;
 
-public class ArrayDemo4 {
-    public static void main(String[] args) {
-        //动态数组使用
-        double[] scores = new double[3];
-
-        //赋值
 
     }
     public static void main(String[] args) {
