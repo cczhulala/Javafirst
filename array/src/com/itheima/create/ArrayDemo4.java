8b8d6d85939c9a1b904c1510f919ebe9bc890ee5
ccz <changzhichen@outlook.com>
2022-12-25 16:05:21 +0800

    third commit

--- array/src/com/itheima/create/ArrayDemo4.java
+++ array/src/com/itheima/create/ArrayDemo4.java
@@ -11,12 +11,7 @@ public class ArrayDemo4 {
         System.out.println(scores[1]);
     }
     public static void main(String[] args) {
-        //动态数组使用
-        double[] scores = new double[3];
 
-        //赋值
-        scores[0] = 99.5;
-        System.out.println(scores[0]);
-        System.out.println(scores[1]);
+
     }
 }
