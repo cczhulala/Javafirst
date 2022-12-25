7b743bab4ab604529fdfbea078ffd48938659e3c
ccz <changzhichen@outlook.com>
2022-12-25 15:55:14 +0800

    first commit

---
+++ array/src/com/itheima/create/ArrayDemo4.java
@@ -0,0 +1,13 @@
+package com.itheima.create;
+
+public class ArrayDemo4 {
+    public static void main(String[] args) {
+        //动态数组使用
+        double[] scores = new double[3];
+
+        //赋值
+        scores[0] = 99.5;
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+    }
+}
